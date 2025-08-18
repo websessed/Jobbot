e0cde4fe1990d1a0ca65e755d48e99e1e5ce3b38
Maxwell Easter <maxwelleaster00@gmail.com>
2025-08-18 07:11:39 +0100

    add desc.js

---
+++ desc.js
@@ -0,0 +1,10 @@
+
+/* 
+v1
+core features: 
+    -user subscription
+    -job fetching & storage 
+    -job notif via telegram messages
+    -user preferences, -view/track sent jobs via telegram commands  
+    -inline job actions (save, open link)
+*/
